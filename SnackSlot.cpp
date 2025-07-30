ddce55a5343a1a2063e4f2657e7118da1c65d2c8
Web-maxim <72805532+Web-maxim@users.noreply.github.com>
2025-07-30 21:52:29 +0400

    Add files via upload
    Signed-off-by: Web-maxim <72805532+Web-maxim@users.noreply.github.com>

---
+++ SnackSlot.cpp
@@ -0,0 +1,35 @@
+﻿// SnackSlot.cpp
+#include "SnackSlot.h"
+
+SnackSlot::SnackSlot(int capacity) : capacity(capacity) {}
+
+SnackSlot::~SnackSlot() {
+    // Слот не владеет снеками, не удаляем их здесь
+}
+
+bool SnackSlot::addSnack(Snack* snack) {
+    if ((int)snacks.size() < capacity) {
+        snacks.push_back(snack);
+        return true;
+    }
+    return false;
+}
+
+int SnackSlot::getCount() const {
+    return static_cast<int>(snacks.size());
+}
+
+int SnackSlot::getCapacity() const {
+    return capacity;
+}
+
+bool SnackSlot::isEmpty() const {
+    return snacks.empty();
+}
+
+void SnackSlot::setCapacity(int newCapacity) {
+    if (newCapacity >= 0 && newCapacity >= (int)snacks.size()) {
+        capacity = newCapacity;
+    }
+    // Иначе игнорируем, чтобы не потерять уже добавленные снекы
+}
