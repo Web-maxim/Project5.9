881f8295c5c175418576292a390648efff3ef347
Web-maxim <72805532+Web-maxim@users.noreply.github.com>
2025-07-30 21:54:18 +0400

    Add files via upload
    Signed-off-by: Web-maxim <72805532+Web-maxim@users.noreply.github.com>

---
+++ main.cpp
@@ -0,0 +1,26 @@
+﻿// main.cpp
+#include <iostream>
+#include "Snack.h"
+#include "SnackSlot.h"
+#include "VendingMachine.h"
+
+using namespace std;
+
+int main() {    
+    Snack* bounty = new Snack("Bounty");
+    Snack* snickers = new Snack("Snickers");
+    SnackSlot* slot = new SnackSlot(10);  /*количество батончиков, которые помещаются в слот*/
+    slot->addSnack(bounty);  //Добавляем батончик в слот
+    slot->addSnack(snickers);
+    VendingMachine* machine = new VendingMachine(5);  /*Количество слотов для снеков*/
+    machine->addSlot(slot);  // Помещаем слот обратно в аппарат
+
+    cout << machine->getEmptySlotsCount() << endl; // Должно вывести 0, т.к. слот не пустой
+
+    delete machine;
+    delete slot;
+    delete snickers;
+    delete bounty;
+
+    return 0;
+}
